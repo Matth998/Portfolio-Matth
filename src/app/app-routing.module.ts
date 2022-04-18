a29ec450f21b1cc189c2bd2198c1c02499be4797
Matth998 <matheusoliveira.3125@gmail.com>
2022-04-17 21:48:21 -0300

    Page 404.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { Error404Component } from './error404/error404.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+
+  {path: 'home', component: HomeComponent},
+  
+  {path: '**', component: Error404Component}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
