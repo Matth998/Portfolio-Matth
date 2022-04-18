950da1586865cd526d7348f4f41159f653e43e1b
Matth998 <matheusoliveira.3125@gmail.com>
2022-04-17 23:11:04 -0300

    Home and About me

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
 import { Error404Component } from './error404/error404.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { Error404Component } from './error404/error404.component';
     HomeComponent,
     MenuComponent,
     FooterComponent,
-    Error404Component
+    Error404Component,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
